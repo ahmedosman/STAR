5e620f780515ed523094ec8266f59b7375a2abed
neoglez <neoglez@gmail.com>
2020-10-16 12:11:51 +0200

    demo load chumpy

--- demo/load_chumpy.py
+++ demo/load_chumpy.py
@@ -19,5 +19,22 @@
 # Code Developed by:
 # Ahmed A. A. Osman
 
-from ch.star import STAR
+from STAR.ch.star import STAR
+import numpy as np
+
 model = STAR(gender='female',num_betas=10)
+## Assign random pose and shape parameters
+model.pose[:] = np.random.rand(model.pose.size) * .2
+model.betas[:] = np.random.rand(model.betas.size) * .03
+
+## Write to an .obj file
+outmesh_path = './hello_smpl.obj'
+with open( outmesh_path, 'w') as fp:
+    for v in model.r:
+        fp.write( 'v %f %f %f\n' % ( v[0], v[1], v[2]) )
+
+    for f in model.f+1: # Faces are 1-based, not 0-based in obj files
+        fp.write( 'f %d %d %d\n' %  (f[0], f[1], f[2]) )
+
+## Print message
+print ('..Output mesh saved to: {}'.format(outmesh_path))
